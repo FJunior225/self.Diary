6cb2eb43fcf74301e7423e68d2da9a2e9e691078
FJ Collins Jr <fcollins@live.com>
2016-05-10 15:11:15 -0400

    add columns to entries, add association to user and validation for columns to be present

--- db/migrate/20160510182258_create_entries.rb
+++ db/migrate/20160510182258_create_entries.rb
@@ -1,6 +1,11 @@
 class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
+      t.string :entry_title, null: false
+      t.string :emotions
+      t.string :event
+      t.string :thoughts
+      t.string :reality
 
       t.timestamps null: false
     end
